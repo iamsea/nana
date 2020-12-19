6a4489a9ec00e0af088ea7dd1c7a85aa163a29d8
1984 <mac@1984deMacBook-Pro.local>
2020-12-19 20:47:12 +0800

    fix bug

--- nana.ts
+++ nana.ts
@@ -3,8 +3,5 @@ console.log('hi nana');
 console.log('nana back master');
 
 console.log('nana update');
-<<<<<<< HEAD
-console.log(hello world)
-console.log('hello space')
-=======
->>>>>>> parent of f8dd34d... nana say hello world
+console.log('hello world');
+console.log('hello space');
