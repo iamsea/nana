b4cd1f63051b2d4a6349c14f7a3fd14dc2d5e70c
1984 <mac@1984deMacBook-Pro.local>
2020-12-19 20:19:59 +0800

    revert to say hello wolrd

--- nana.ts
+++ nana.ts
@@ -3,5 +3,8 @@ console.log('hi nana');
 console.log('nana back master');
 
 console.log('nana update');
+<<<<<<< HEAD
 console.log(hello world)
 console.log('hello space')
+=======
+>>>>>>> parent of f8dd34d... nana say hello world
